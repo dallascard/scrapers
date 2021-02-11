04d84246bc52943b503899a3d31a889b23c0ce86
dallascard <dallas.card@gmail.com>
2021-02-11 10:35:37 -0800

    refactoring all to use common function

--- app/scrape_inaugurals.py
+++ app/scrape_inaugurals.py
@@ -1,84 +1,32 @@
-import os
-import re
-import json
-import time
 from optparse import OptionParser
 
-from bs4 import BeautifulSoup
-from common.urllib_get import urllib_get
-from app.common import process_speech
-
+from app.common import scrape_documents
 
 
 def main():
     usage = "%prog\n" \
-            "Scrape Inaugural addresses from the American Presidency Project"
+            "Scrape farewell addresses from the American Presidency Project"
     parser = OptionParser(usage=usage)
-    parser.add_option('--outdir', type=str, default='data/app/inaugural/',
+    parser.add_option('--outdir', type=str, default='data/app/inaugurals/',
                       help='Output directory): default=%default')
-    parser.add_option('--pages', type=int, default=7,
-                      help='Number of pages of results: default=%default')
-    #parser.add_option('--by-issue', action="store_true", default=False,
-    #                  help='Divide data by issue: default=%default')
+    parser.add_option('--sleep', type=int, default=2,
+                      help='Seconds to sleep after downloading a page: default=%default')
+    parser.add_option('--overwrite', action="store_true", default=False,
+                      help='Overwrite index pages: default=%default')
+    parser.add_option('--overwrite-all', action="store_true", default=False,
+                      help='Overwrite index and document pages: default=%default')
 
     (options, args) = parser.parse_args()
 
-    pages = options.pages
     outdir = options.outdir
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-
-    raw_dir = os.path.join(outdir, 'raw')
-    if not os.path.exists(raw_dir):
-        os.makedirs(raw_dir)
+    overwrite = options.overwrite
+    overwrite_all = options.overwrite_all
+    sleep = options.sleep
 
     domain = 'https://www.presidency.ucsb.edu'
+    initial_target = '/documents/app-categories/spoken-addresses-and-remarks/presidential/inaugural-addresses'
 
-    outlines = []
-    for page in range(pages):
-        if page == 0:
-            url = 'https://www.presidency.ucsb.edu/documents/app-categories/spoken-addresses-and-remarks/presidential/inaugural-addresses'
-            index_file = os.path.join(outdir, 'index.html')
-        else:
-            url = 'https://www.presidency.ucsb.edu/documents/app-categories/spoken-addresses-and-remarks/presidential/inaugural-addresses?page=' + str(page)
-            index_file = os.path.join(outdir, 'index?page' + str(page) + '.html')
-
-        if os.path.exists(index_file):
-            print("Loading index page")
-            with open(index_file, 'rb') as f:
-                raw_html = f.read()
-        else:
-            print("Downloading index page")
-
-            raw_html = urllib_get(url)
-            print("Saving index page")
-            with open(index_file, 'wb') as f:
-                f.write(raw_html)
-
-        html = BeautifulSoup(raw_html, 'html.parser')
-
-        divs = html.findAll('div', {'class': ['field-title', 'col-sm-4']})
-
-        link = None
-        for d_i, div in enumerate(divs):
-            links = div.findAll('a', href=True)
-            if len(links) > 0:
-                if d_i % 2 == 0:
-                    link = links[0]['href']
-                    print(d_i, link)
-                else:
-                    name = links[0].text.strip()
-                    print(d_i, name)
-                    assert link is not None
-                    output = {'person': name, 'url': domain + link}
-                    print("Scraping", domain + link)
-                    output.update(process_speech(domain + link, raw_dir, verbose=False))
-                    outlines.append(output)
-
-    print("Saving {:d} inaugurals".format(len(outlines)))
-    with open(os.path.join(outdir, 'inaugurals.all.jsonlist'), 'w') as fo:
-        for line in outlines:
-            fo.write(json.dumps(line) + '\n')
+    scrape_documents(initial_target, outdir, overwrite_index_pages=overwrite, overwrite_all=overwrite_all, sleep=sleep, domain=domain)
 
 
 if __name__ == '__main__':
