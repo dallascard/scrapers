b186d773b94b9eacc8119573108127919ebf03ad
dallascard <dallas.card@gmail.com>
2021-02-14 10:59:29 -0800

    adding script for press briefings

---
+++ app/scrape_press_briefings.py
@@ -0,0 +1,33 @@
+from optparse import OptionParser
+
+from app.common import scrape_documents
+
+
+def main():
+    usage = "%prog\n" \
+            "Scrape press briefings from the American Presidency Project"
+    parser = OptionParser(usage=usage)
+    parser.add_option('--outdir', type=str, default='data/app/press_briefings/',
+                      help='Output directory): default=%default')
+    parser.add_option('--sleep', type=int, default=2,
+                      help='Seconds to sleep after downloading a page: default=%default')
+    parser.add_option('--overwrite', action="store_true", default=False,
+                      help='Overwrite index pages: default=%default')
+    parser.add_option('--overwrite-all', action="store_true", default=False,
+                      help='Overwrite index and document pages: default=%default')
+
+    (options, args) = parser.parse_args()
+
+    outdir = options.outdir
+    overwrite = options.overwrite
+    overwrite_all = options.overwrite_all
+    sleep = options.sleep
+
+    domain = 'https://www.presidency.ucsb.edu'
+    initial_target = '/documents/app-categories/pressmedia/press-briefings'
+
+    scrape_documents(initial_target, outdir, overwrite_index_pages=overwrite, overwrite_all=overwrite_all, sleep=sleep, domain=domain)
+
+
+if __name__ == '__main__':
+    main()
