9ea34ddf09bb04f0cadb3821b7c553126b0b1246
dallascard <dallas.card@gmail.com>
2021-02-17 15:23:06 -0800

    adding script to split file

---
+++ app/split_into_pieces.py
@@ -0,0 +1,37 @@
+import os
+import json
+from optparse import OptionParser
+from collections import defaultdict, Counter
+
+
+def main():
+    usage = "%prog infile.jsonlist"
+    parser = OptionParser(usage=usage)
+    parser.add_option('--pieces', type=int, default=5,
+                      help='Number of pieces to break the file into: default=%default')
+    #parser.add_option('--by-issue', action="store_true", default=False,
+    #                  help='Divide data by issue: default=%default')
+
+    (options, args) = parser.parse_args()
+
+    infile = args[0]
+
+    n_pieces = options.pieces
+
+    with open(infile) as f:
+        lines = f.readlines()
+
+    size = len(lines) // n_pieces
+
+    for p in range(n_pieces):
+        if p == n_pieces-1:
+            outlines = lines[p * size:]
+        else:
+            outlines = lines[p * size: (p+1) * size]
+        with open(infile[-9:] + '_' + str(p) + '.jsonlist', 'w') as f:
+            for line in outlines:
+                f.write(line)
+
+
+if __name__ == '__main__':
+    main()
