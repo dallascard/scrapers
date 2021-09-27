2ef6bc9d6820a051b0eeae17767fa323a12add84
dallascard <dallas.card@gmail.com>
2021-09-27 15:57:33 -0700

    gunzip

---
+++ library_of_congress/gunzip_files.py
@@ -0,0 +1,30 @@
+import os
+import json
+from glob import glob
+from subprocess import call
+from optparse import OptionParser
+from collections import defaultdict, Counter
+
+# Download the public database of bibliographic metadata from the Library of Congress
+
+def main():
+    usage = "%prog"
+    parser = OptionParser(usage=usage)
+    parser.add_option('--basedir', type=str, default='data/iclr/',
+                      help='Data directory: default=%default')
+    #parser.add_option('--by-issue', action="store_true", default=False,
+    #                  help='Divide data by issue: default=%default')
+
+    (options, args) = parser.parse_args()
+
+    basedir = options.basedir
+    files = sorted(glob(os.path.join(basedir, '*.gz')))
+
+    for infile in files:
+        cmd = ['gunzip', infile]
+        print(' '.join(cmd))
+        call(cmd)
+
+
+if __name__ == '__main__':
+    main()
