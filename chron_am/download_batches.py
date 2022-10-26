4c30c6863281a881be777358a0c10e463d3d5a66
Dallas Card <dallas.card@gmail.com>
2022-10-26 12:32:57 -0400

    fix

--- chron_am/download_batches.py
+++ chron_am/download_batches.py
@@ -103,6 +103,10 @@ def main():
         else:
             attempts = 0
             destination_file = os.path.join(tar_files_dir, filename)
+            
+            if overwrite and os.path.exists(destination_file):
+                os.remove(destination_file)
+            
             while not os.path.exists(destination_file):
                 command = ['wget', url, '-P', tar_files_dir]
                 print("Downloading from", url, "(attempt {:d}".format(attempts))
